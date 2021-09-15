f55a7b298be86472de344a0bfa7fe98466f0e4f2
Franck Kabasele <kabasele467@gmail.com>
2021-09-15 16:53:05 -0400

    working with query middleware

--- models/tour.js
+++ models/tour.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
-// const slugify = require('slugify');
+const slugify = require('slugify');
 // const validator = require('validator');
+const opts = {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+};
 
 const tourSchema = new mongoose.Schema(
   {
@@ -80,11 +84,32 @@ const tourSchema = new mongoose.Schema(
       default: false,
     },
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-}
+  opts
 );
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
