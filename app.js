bd14d9c12e7f3cf07b182cc59427da0e3b03d592
Franck Kabasele <kabasele467@gmail.com>
2021-09-16 13:56:40 -0400

    Add Global error handler

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controller/errorController');
 const tourRouter = require('./routes/tours');
 const userRouter = require('./routes/users');
 
@@ -18,4 +20,11 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+app.use(globalErrorHandler);
 module.exports = app;
--- models/tour.js
+++ models/tour.js
@@ -39,7 +39,7 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0']
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -52,7 +52,7 @@ const tourSchema = new mongoose.Schema(
     priceDiscount: {
       type: Number,
       validate: {
-        validator: function(val) {
+        validator: function (val) {
           // this only points to current doc on NEW document creation
           return val < this.price;
         },
