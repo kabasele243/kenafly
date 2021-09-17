737cc47f4535771666bde5924415fd198247eb70
Franck Kabasele <kabasele467@gmail.com>
2021-09-16 20:45:00 -0400

    Start User Ressource

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+});
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const user = require('../controller/users');
+const auth = require('../controller/auth');
 
 const router = express.Router();
+
+router.post('/signup', auth.signup);
+// router.post('/login', auth.login);
 
 router.route('/').get(user.getAllUsers).post(user.createUser);
 
