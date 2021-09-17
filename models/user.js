80dd88d000e22b06df236ea29b445686a6fc4bb3
Franck Kabasele <kabasele467@gmail.com>
2021-09-17 15:29:04 -0400

    Implemented Protected Route

--- models/user.js
+++ models/user.js
@@ -6,14 +6,14 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please tell us your name!']
+    required: [true, 'Please tell us your name!'],
   },
   email: {
     type: String,
     required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email']
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
   role: {
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema({
       validator: function (passwordConfirm) {
         return passwordConfirm === this.password;
       },
-      message: 'Passwords are not the same!'
+      message: 'Passwords are not the same!',
     },
   },
   passwordChangedAt: Date,
@@ -65,6 +65,35 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  // False means NOT changed
+  return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
--- routes/tours.js
+++ routes/tours.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const tour = require('../controller/tours');
+const auth = require('../controller/auth');
 
 const router = express.Router();
 
-router.route('/').get(tour.getAllTours).post(tour.createTour);
+router.route('/top-5-cheap').get(tour.aliasTopTours, tour.getAllTours);
+
+router.route('/tour-stats').get(tour.getTourStats);
+router.route('/monthly-plan/:year').get(tour.getMonthlyPlan);
+
+router.route('/').get(auth.protect, tour.getAllTours).post(tour.createTour);
 
 router.route('/tour-stats').get(tour.getTourStats);
 router.route('/monthly-plan/:year').get(tour.getMonthlyPlan);
